6d143c960e4f6c50bfebe9657a8ca0787e03230d
Oluwafemi Korede lawrence <o.korede@exolvemedia.com>
2014-04-04 01:25:45 +0100

    initial commit
    yes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var app = {
+    showAlert: function(message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    }
+};
